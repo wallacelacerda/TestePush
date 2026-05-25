c95879f2b136f505911f112a18ed64b078950610
wallace lacerda <wallace.lacerda@aluno.senac.rio>
2026-05-25 19:43:58 -0300

    quarto commit

---
+++ exemplo.py
@@ -0,0 +1,7 @@
+ExemploAula4 = "commit"
+Exemplo2 = 1
+Exemplo3 = 2
+exemplo4 = 3
+exemplo5 = 4
+git config --global user.nome: 'wallacelacerda'
+git config --global user.email: 'wallace.lacerda@aluno.senac.rio'
