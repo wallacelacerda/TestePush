e6f8233c87698c74fef6e461b18edd0c8fa5be2a
wallacelacerda <wallace.lacerda@aluno.senac.rio>
2026-05-25 21:06:40 -0300

    Update exemplo.py
    Quinto commit

--- exemplo.py
+++ exemplo.py
@@ -3,5 +3,6 @@ Exemplo2 = 1
 Exemplo3 = 2
 exemplo4 = 3
 exemplo5 = 4
+exemplo6 = 5
 git config --global user.nome: 'wallacelacerda'
 git config --global user.email: 'wallace.lacerda@aluno.senac.rio'
